94512482e2104c798b17aa6968a56560a63f779c
Andrei Borisov <charonas@yandex.ru>
2024-08-26 17:20:54 +0300

    правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEWSURNAME = "";
+    public String surname;
+    String name;
+    private int sizeOfempty = 10;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+}
