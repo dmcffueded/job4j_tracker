6fbd8168e04daefd4032319df2b8895863b6124d
Andrei Borisov <charonas@yandex.ru>
2024-08-30 08:57:44 +0300

    правка [#122642 #511034]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -30,8 +30,6 @@ public class Triangle {
         if (this.exist(ab, ac, bc)) {
             double p = semiPerimeter(ab, ac, bc);
             result = sqrt(p * (p - ab) * (p - bc) * (p - ac));
-        } else {
-            result = -1;
         }
         return result;
     }
