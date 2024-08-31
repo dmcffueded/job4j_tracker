739737e4d62f559926274e7dd1c03b9277d3fe12
Andrei Borisov <charonas@yandex.ru>
2024-08-31 11:54:06 +0300

    метод удаления заявки delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -41,6 +41,17 @@ public class Tracker {
         return false;
     }
 
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            System.arraycopy(items, 0, items, 0, 0);
+        } else {
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
+        size--;
+        }
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
