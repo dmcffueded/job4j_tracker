d605ebafd7c898646e17119cb7edf87549210bb6
Andrei Borisov <charonas@yandex.ru>
2024-09-01 07:48:26 +0300

    правка [#211749 #511343]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -43,9 +43,7 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            System.arraycopy(items, 0, items, 0, 0);
-        } else {
+        if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
