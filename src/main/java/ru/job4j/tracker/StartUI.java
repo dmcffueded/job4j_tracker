33c9a70ace46ba9fe767eb617bf0a0e878c45914
Andrei Borisov <charonas@yandex.ru>
2024-09-02 15:29:09 +0300

    Вывод меню класса StartUI [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,16 @@
 package ru.job4j.tracker;
 
+/*
+package ru.job4j.tracker;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/*
 создаем объект класса Item
 на этом объекте вызываем геттер и получаем объект LocalDateTime
 выбираем паттерн форматирования DateTimeFormatter
 форматируем
 результат выводим в строку
-*/
 
 public class StartUI {
     public static void main(String[] args) {
@@ -19,5 +20,43 @@ public class StartUI {
         String currentDateTimeFormat = currentDateTime.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
         System.out.println(itm);
+    }
+}
+*/
+
+import java.util.Scanner;
+
+public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
