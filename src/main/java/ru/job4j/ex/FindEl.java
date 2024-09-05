f1c34c23dd8f3f4e51f25f68e0c1389a2c42aa46
Andrei Borisov <charonas@yandex.ru>
2024-09-05 17:47:39 +0300

    пользовательские исключения [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"test1", "test2"};
+        String key1 = "test1";
+        String key2 = "test3";
+        try {
+            indexOf(value, key1);
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            indexOf(value, key1);
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
