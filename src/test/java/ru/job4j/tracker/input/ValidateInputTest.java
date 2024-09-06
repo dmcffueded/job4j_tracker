737aff475879e8e40e4c63b36d1617452cf2a409
Andrei Borisov <charonas@yandex.ru>
2024-09-06 14:47:33 +0300

    тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.tracker.input;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenMultipleValidInputs() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+
+        int firstSelected = input.askInt("Enter menu:");
+        assertThat(firstSelected).isEqualTo(1);
+
+        int secondSelected = input.askInt("Enter menu:");
+        assertThat(secondSelected).isEqualTo(2);
+
+        int thirdSelected = input.askInt("Enter menu:");
+        assertThat(thirdSelected).isEqualTo(3);
+    }
+}
