21ed01cd1b67ea3fd7f7c24415b914882c613095
Andrei Borisov <charonas@yandex.ru>
2024-09-06 06:00:52 +0300

    добавил тест на корректный результат [#219366 #512014]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -16,4 +16,11 @@ class FactorialTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    public void whenWithoutException() {
+        int result = Factorial.calc(3);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
 }
