5982ad7746cb29fe25b0ee043dfac7db9f98508b
Andrei Borisov <charonas@yandex.ru>
2024-09-27 16:03:00 +0300

    Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name1@mail.ru", "Ivan Ivanov");
+        map.put("name2@mail.ru", "Sergei Sergeev");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
