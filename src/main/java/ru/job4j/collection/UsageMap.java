ef7290dc348a2491c3f9f7ebdb6f0c684df4501c
Andrei Borisov <charonas@yandex.ru>
2024-09-28 10:15:54 +0300

    добавил пары

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("name1@mail.ru", "Ivan Ivanov");
         map.put("name2@mail.ru", "Sergei Sergeev");
+        map.put("name3@mail.ru", "Sergei Ivanov");
+        map.put("name4@mail.ru", "Ivan Sergeev");
+        map.put("name2@mail.ru", "Ivan Sergeev");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
