a76b1128dd68d0693492d7f214c8e1472ab4377a
Andrei Borisov <charonas@yandex.ru>
2024-10-17 16:48:50 +0300

    правка 1

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -9,7 +9,7 @@ public class ListToMap {
     public static Map<String, Student> convert(List<Student> list) {
         return list.stream()
                 .collect(Collectors.toMap(
-                        element -> element.getSurname(),
+                        Student::getSurname,
                         element -> element,
                         (existing, replacement) -> existing
                 ));
