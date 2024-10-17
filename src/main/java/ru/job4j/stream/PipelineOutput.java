32d149d954f3e8a777d7770f4ea0f6b14883f2bb
Andrei Borisov <charonas@yandex.ru>
2024-10-17 20:38:17 +0300

    Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/PipelineOutput.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class PipelineOutput {
+    public static void main(String[] args) {
+        Stream.of("one", "two", "three")
+                .filter(x -> {
+                    System.out.print(x);
+                    return x.length() <= 3;
+                })
+                .map(x -> {
+                    System.out.println(x);
+                    return x.toUpperCase();
+                })
+                .forEach(System.out::println);
+    }
+}
